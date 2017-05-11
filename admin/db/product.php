063cd760ed71defa2ce8647287b7e4fb6698d6db
tranviethai <haitranviet96@gmail.com>
2017-05-11 11:33:12 +0700

    update search order

--- admin/db/product.php
+++ admin/db/product.php
@@ -17,3 +17,29 @@ function product_add($data = array()) {
 function product_delete($id) {
     return db_delete_by_id('product', 'id', $id);
 }
+if (isset($_GET['fproduct'])){
+    ob_start();
+    require_once '../../system/database.php';
+    global $conn;
+    db_connect();
+        $fplace = mysqli_real_escape_string($conn, $_GET['fproduct']);
+        $product = $_GET['fproduct'];
+        $query = 'SELECT * FROM product WHERE name like "%'.$product.'%"';
+//        mysqli_select_db($conn, "user");
+//        $result = mysqli_query($conn, $query);
+//        $count = mysqli_num_rows($result);
+        $result = mysqli_query($conn,$query);
+        $count = mysqli_num_rows($result);
+        
+        if ($count > 0) {
+            $json = [];
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                array_push($json, $row);
+            }
+            $json = json_encode($json);
+            header('Content-Type: application/json');
+            echo $json;
+        }
+        db_disconnect();
+        ob_end_flush();
+    }
